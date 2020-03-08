5c65c7a6d252d0dd6cbf71263bc30a4f3e8ff47c
Zhi Qu <zhiqu@stripe.com>
2020-03-08 16:13:15 -0700

    Pakckage setup

---
+++ helper/genPic.go
@@ -0,0 +1,14 @@
+package helper
+
+// Pic Generate a picture
+func Pic(dx, dy int) [][]uint8 {
+	result := make([][]uint8, dy)
+	for iy := 0; iy < dx; iy++ {
+		result[iy] = make([]uint8, dx)
+		for ix := 0; ix < dx; ix++ {
+			result[iy][ix] = uint8(ix) * uint8(iy)
+		}
+
+	}
+	return result
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"GolangPlayground/helper"
+	"fmt"
+
+	"golang.org/x/tour/pic"
+)
+
+func main() {
+	fmt.Println(helper.Sqrt(2))
+	pic.Show(helper.Pic)
+}
